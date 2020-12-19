5dca5cdbc1cde1972d6abfe52b63e0ba0be086cd
s0umyajeet <mandal.soumyajeet@gmail.com>
2020-12-19 22:01:55 +0530

    [wip] | Game Engine header file

--- include/Game.h
+++ include/Game.h
@@ -1,22 +1,41 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <SDL2/SDL.h>
 
 class Game
 {
     public:
         Game();
 
-        void init();
+        //initalize the SDL subsystems, create a window and a renderer
+        void init(string title, int x, int y, int width, int height, int flags = 0);
+
+        //render the required elements onto the screen
         void render();
+
+        //event handling for input etc.
         void handleEvents();
+
+        //game phycics
+        void update();
+
+        //error logging
+        void errorLogger();
+
+        //cleaing the game
         void clean();
+
+        //for debug
         void greet();
 
-        bool isRunning() { return _isRunning; }
+        //is the game loop running?
+        inline bool isRunning() { return m_isRunning; }
 
     private:
-        bool _isRunning;
+        bool m_isRunning;
+        SDL_Window *m_window;
+        SDL_Renderer *m_renderer;
 };
 
 #endif // GAME_H
