7a2d175850b9f4066410c978082d5615ae7a4aee
s0umyajeet <mandal.soumyajeet@gmail.com>
2020-12-19 22:15:41 +0530

    [wip] | minor debugging

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@ Game* gameObj = new Game();
 
 int main(int argc, char* argv[]) {
 
-    string windowTitle = "Game Window";
+    std::string windowTitle = "Game Window";
     int win_x = SDL_WINDOWPOS_CENTERED;
     int win_y = SDL_WINDOWPOS_CENTERED;
     int width = 800;
     int height = 600;
 
-    gameObj->init(windowTitle, win_x, win_y, width, height, NULL)
+    gameObj->init(windowTitle, win_x, win_y, width, height, NULL);
 
     while (gameObj->isRunning()) {
         gameObj->handleEvents();
