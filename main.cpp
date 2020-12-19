e9089b06aa8bc78dff73bf0af08abf9342bf52ca
s0umyajeet <mandal.soumyajeet@gmail.com>
2020-12-19 21:53:24 +0530

    [wip] | created game loop

--- main.cpp
+++ main.cpp
@@ -6,36 +6,20 @@ Game* gameObj = new Game();
 
 int main(int argc, char* argv[]) {
 
-//    while (gameObj->isRunning()) {
-//
-//    }
+    string windowTitle = "Game Window";
+    int win_x = SDL_WINDOWPOS_CENTERED;
+    int win_y = SDL_WINDOWPOS_CENTERED;
+    int width = 800;
+    int height = 600;
 
-    gameObj->greet();
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        std::cout << "Error initializing SDL" << std::endl;
-        return 0;
+    gameObj->init(windowTitle, win_x, win_y, width, height, NULL)
+
+    while (gameObj->isRunning()) {
+        gameObj->handleEvents();
+        gameObj->update();
+        gameObj->render();
     }
 
-    SDL_Window *m_window =
-    SDL_CreateWindow("hello SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, NULL);
-
-    if (m_window == NULL) {
-        std::cout << "Error creating window" << std::endl;
-        return 0;
-    }
-
-    SDL_Renderer* m_renderer = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED);
-
-    if (m_renderer == NULL) {
-        std::cout << "Error creating renderer" << std::endl;
-        return 0;
-    }
-
-    while (1) {
-        SDL_SetRenderDrawColor(m_renderer, 0, 255, 255, 255);
-        SDL_RenderClear(m_renderer);
-        SDL_RenderPresent(m_renderer);
-    }
-
+    gameObj->clean();
     return 0;
 }
