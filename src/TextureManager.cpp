1d539b584847bb7cccaf8adefdcbe82bd90722ad
s0umyajeet <mandal.soumyajeet@gmail.com>
2020-12-20 13:36:06 +0530

    [checkpoint] | Added method implementations for Texture Manager class (also made it singleton)

---
+++ src/TextureManager.cpp
@@ -0,0 +1,74 @@
+#include "TextureManager.h"
+
+TextureManager::TextureManager() { }
+
+TextureManager TextureManager::m_instance;
+
+TextureManager& TextureManager::getInstance()
+{
+    return m_instance;
+}
+
+bool TextureManager::load(std::string fileName, std::string id, SDL_Renderer* renderer)
+{
+    SDL_Surface *tempSurface = IMG_Load(fileName.c_str());
+
+    if (tempSurface == NULL) {
+        //Error manager code here
+        return false;
+    }
+
+    SDL_Texture* tempTexture = SDL_CreateTextureFromSurface(renderer, tempSurface);
+
+    //Let go of the temporary surface
+    SDL_FreeSurface(tempSurface);
+
+    if (tempTexture == NULL) {
+        //Error manager code here
+        return false;
+    }
+
+    m_texMap[id] = tempTexture;
+
+    return true;
+}
+
+void TextureManager::draw(std::string id, int x, int y, int width, int height, SDL_Renderer* renderer, SDL_RendererFlip flip)
+{
+    SDL_Rect srcRect;
+    SDL_Rect dstRect;
+
+    //for source rectangle
+    srcRect.x = srcRect.y = 0;
+    srcRect.w = width;  // Texture width
+    srcRect.h = height;  // Texture height
+
+    //for destination rectangle
+    dstRect.x = x;      //coordinates of where to draw on the screen
+    dstRect.y = y;      //coordinates of where to draw on the screen
+    dstRect.w = width;  //destination width same as texture width (basically copying the whole texture)
+    dstRect.h = height; //destination height same as texture height (-------------11------------------)
+
+    SDL_RenderCopyEx(renderer, m_texMap[id], &srcRect, &dstRect, 0, 0, flip);
+
+}
+
+void TextureManager::drawFrame(std::string id, int x, int y, int width, int height, int currentRow, int currentFrame, SDL_Renderer* renderer, SDL_RendererFlip flip)
+{
+    SDL_Rect srcRect;
+    SDL_Rect dstRect;
+
+    //for source rectangle
+    srcRect.x = width * currentFrame;
+    srcRect.y = height * currentRow;
+    srcRect.w = width;
+    srcRect.h = height;
+
+    //for destinantion rectangle
+    dstRect.x = x;
+    dstRect.y = y;
+    dstRect.w = width;
+    dstRect.h = height;
+
+    SDL_RenderCopyEx(renderer, m_texMap[id], &srcRect, &dstRect, 0, 0, flip);
+}
