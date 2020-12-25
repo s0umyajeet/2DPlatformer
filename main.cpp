57e0485e9fd8752921723cba2ebc367e937e6320
s0umyajeet <mandal.soumyajeet@gmail.com>
2020-12-25 10:15:30 +0530

    [wip] | Changed screen size and added delay to cap fps

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ int main(int argc, char* argv[]) {
     std::string windowTitle = "Game Window";
     int win_x = SDL_WINDOWPOS_CENTERED;
     int win_y = SDL_WINDOWPOS_CENTERED;
-    int width = 800;
-    int height = 600;
+    int width = 1000;
+    int height = 800;
 
     Game::get().init(windowTitle, win_x, win_y, width, height, NULL);
 
@@ -14,6 +14,7 @@ int main(int argc, char* argv[]) {
         Game::get().handleEvents();
         Game::get().update();
         Game::get().render();
+        SDL_Delay(10);
     }
 
     Game::get().clean();
