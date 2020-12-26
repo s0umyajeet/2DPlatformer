707057331773cf68f0013be33ed6eab4837124f5
s0umyajeet <mandal.soumyajeet@gmail.com>
2020-12-26 19:46:41 +0530

    [wip] | Added mouse click support

--- src/InputHandler.cpp
+++ src/InputHandler.cpp
@@ -1,8 +1,13 @@
 #include "InputHandler.h"
+#include <Game.h>
 
 InputHandler InputHandler::m_instance;
 
-InputHandler::InputHandler() { }
+InputHandler::InputHandler() {
+    for (int i = 0; i < 3; i++) {
+        m_mouseButtonStates.push_back(false);
+    }
+}
 
 InputHandler& InputHandler::get()
 {
@@ -10,3 +15,61 @@ InputHandler& InputHandler::get()
 }
 
 
+void InputHandler::update()
+{
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            Game::get().Quit();
+        }
+
+        //mouse button down
+        if (event.type == SDL_MOUSEBUTTONDOWN) {
+            switch(event.button.button) {
+            case SDL_BUTTON_LEFT:
+                m_mouseButtonStates[LEFT] = true;
+                break;
+            case SDL_BUTTON_MIDDLE:
+                m_mouseButtonStates[MIDDLE] = true;
+                break;
+            case SDL_BUTTON_RIGHT:
+                m_mouseButtonStates[RIGHT] = true;
+                break;
+            default: break;
+            }
+        }
+
+        //mouse button up
+        if (event.type == SDL_MOUSEBUTTONUP) {
+            switch(event.button.button) {
+            case SDL_BUTTON_LEFT:
+                m_mouseButtonStates[LEFT] = false;
+                break;
+            case SDL_BUTTON_MIDDLE:
+                m_mouseButtonStates[MIDDLE] = false;
+                break;
+            case SDL_BUTTON_RIGHT:
+                m_mouseButtonStates[RIGHT] = false;
+                break;
+            default: break;
+            }
+        }
+
+    }
+}
+
+bool InputHandler::getMouseButtonState(int buttonNumber) {
+    if (buttonNumber < 0 || buttonNumber > 2) {
+        LogManager::get().logMessage("Invalid button (should be 0, 1, 2)", ERROR);
+        Game::get().Quit();
+        return false;
+    } else {
+        return m_mouseButtonStates[buttonNumber];
+    }
+}
+
+void InputHandler::clean()
+{
+
+}
