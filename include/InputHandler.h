05265c7786c480e5a6d16b31fa72c9dc93395ebf
s0umyajeet <mandal.soumyajeet@gmail.com>
2020-12-26 19:46:24 +0530

    [wip] | Added mouse click support

--- include/InputHandler.h
+++ include/InputHandler.h
@@ -1,16 +1,27 @@
 #ifndef INPUTHANDLER_H
 #define INPUTHANDLER_H
 
+#include <SDL2/SDL.h>
+#include <vector>
+#include <LogManager.h>
 
 class InputHandler
 {
     public:
-        InputHandler(const InputHandler) = delete;
+        InputHandler(const InputHandler&) = delete;
         static InputHandler& get();
+        void update();
+        void clean();
+        bool getMouseButtonState(int buttonNumber);
 
     private:
         InputHandler();
         static InputHandler m_instance;
+        std::vector<bool> m_mouseButtonStates;
+};
+
+enum mouseButtons {
+    LEFT, MIDDLE, RIGHT
 };
 
 #endif // INPUTHANDLER_H
